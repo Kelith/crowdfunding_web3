7a7cfd52ff8de0e6db32a247053e5cc0e77b5c39
Kelith <garofalodavide7@gmail.com>
2023-09-22 15:41:13 +0200

    web3 context

---
+++ client/src/context/index.jsx
@@ -0,0 +1,48 @@
+import React, { createContext, useContext } from "react";
+import {
+  useAddress,
+  useContract,
+  useContractWrite,
+  useMetamask,
+} from "@thirdweb-dev/react";
+import { ethers } from "ethers";
+
+const StateContext = createContext();
+
+export const StateContextProvider = ({ children }) => {
+  const { contract } = useContract(
+    "0x8B2080e5d95D01129D45EDf67A2421C8710fA93F"
+  );
+  const { mutateAsync: createCampaign } = useContractWrite(
+    contract,
+    "createCampaign"
+  );
+  const address = useAddress();
+  const connect = useMetamask();
+
+  const publishCampaign = async (form) => {
+    try {
+      const data = await createCampaign([
+        address,
+        form.title,
+        form.description,
+        form.target,
+        new Date(form.deadline).getTime(),
+        form.image,
+      ]);
+
+      console.log("contract call success", data);
+    } catch (e) {
+      console.log("contract call failure", e);
+    }
+  };
+  return (
+    <StateContext.Provider
+      value={{ address, contract, createCampaign: publishCampaign }}
+    >
+      {children}
+    </StateContext.Provider>
+  );
+};
+
+export const useStateContext = () => useContext(StateContext);
